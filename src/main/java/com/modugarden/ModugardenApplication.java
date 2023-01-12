c9f4ed945351a9e9ba1a66b4b64bf843413e899f
kyeong-hyeok <minkh4227@naver.com>
2023-01-12 23:18:04 +0900

    initial commit

---
+++ src/main/java/com/modugarden/ModugardenApplication.java
@@ -0,0 +1,13 @@
+package com.modugarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModugardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModugardenApplication.class, args);
+	}
+
+}
