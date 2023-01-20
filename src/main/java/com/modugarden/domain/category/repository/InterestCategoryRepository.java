3ffe6ff54b58af773481545cbf3d859561f7efcc
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-21 02:56:50 +0900

    [ADD] InterestCategory, UserInterestCategory 레포지토리 추가

---
+++ src/main/java/com/modugarden/domain/category/repository/InterestCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.modugarden.domain.category.repository;
+
+import com.modugarden.domain.category.entity.InterestCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InterestCategoryRepository extends JpaRepository<InterestCategory, Long> {
+    Optional<InterestCategory> findByCategory(String categoryName);
+}
