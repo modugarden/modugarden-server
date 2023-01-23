e0473023a502bd7d883067e0fe9dab691d42d537
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-01-23 12:45:05 +0900

    [ADD/#18] 코드 추가

--- src/main/java/com/modugarden/domain/comment/entity/Comment.java
+++ src/main/java/com/modugarden/domain/comment/entity/Comment.java
@@ -6,29 +6,32 @@ import com.modugarden.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Comment extends BaseTimeEntity {
 
+    //댓글 id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Long commentId;
+    //내용
     @Column(nullable = false)
     private String content;
     //validation 할 때 글자 수 제한 물어보기
-
+    //부모댓글 id
     private Long parentId;
-
+    //포스트 id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id", nullable = false)
-    private Board board;
-
+    private Board boardId;
+    //회원 id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private User userId;
 }
--- src/main/java/com/modugarden/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/modugarden/domain/comment/repository/CommentRepository.java
@@ -1,4 +1,15 @@
 package com.modugarden.domain.comment.repository;
 
-public interface CommentRepository {
+import com.modugarden.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    //댓글 조회
+    List<Comment> list(long boardId);
+    //댓글 작성
+    void write(Comment comment);
+    //댓글 삭제
+    void delete(Comment comment);
 }
