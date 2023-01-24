21e030a02cb3289389712303fffc24f8334666b6
kyeong-hyeok <minkh4227@naver.com>
2023-01-24 13:10:14 +0900

    [ADD] UserProfileImgRequestDto, ResponseDto 생성

---
+++ src/main/java/com/modugarden/domain/user/dto/UserProfileImgResponseDto.java
@@ -0,0 +1,10 @@
+package com.modugarden.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserProfileImgResponseDto {
+    private String profileImage;
+}
