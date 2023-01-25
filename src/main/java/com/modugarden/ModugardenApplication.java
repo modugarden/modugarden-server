069c60231662cda0520b7b1f5a24ee00a059cfc6
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-25 23:40:20 +0900

    [ADD] S3 관련 코드 추가

--- src/main/java/com/modugarden/ModugardenApplication.java
+++ src/main/java/com/modugarden/ModugardenApplication.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ModugardenApplication {
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModugardenApplication.class, args);
