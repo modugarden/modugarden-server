d5846d204a2ef7410c2144e71964d6e77e9ad26c
donghae <donghae1999@gmail.com>
2023-01-26 13:37:59 +0900

    [REFACTOR] 큐레이션 코드 리팩토링

--- src/main/java/com/modugarden/domain/curation/repository/CurationRepository.java
+++ src/main/java/com/modugarden/domain/curation/repository/CurationRepository.java
@@ -10,9 +10,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurationRepository extends JpaRepository<Curation, Long> {
+    //회원 id로 조회
     Page<Curation> findAllByUser_Id(Long user_id, Pageable pageable);
-
+    //카테고리와 제목 검색
     Slice<Curation> findAllByCategoryAndTitleLikeOrderByCreatedDateDesc(InterestCategory category, String title, Pageable pageable);
-
+    //카테고리로 생성일자 순 조회
     Slice<Curation> findAllByCategoryOrderByCreatedDateDesc(InterestCategory category, Pageable pageable);
 }
