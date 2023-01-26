1d92a77bbdf5f2ec66b88188a42f3fcc62ab3774
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-27 01:46:19 +0900

    [ADD] @Valid exception 처리 관련 코드 추가

--- src/main/java/com/modugarden/common/error/enums/ErrorMessage.java
+++ src/main/java/com/modugarden/common/error/enums/ErrorMessage.java
@@ -26,7 +26,8 @@ public enum ErrorMessage {
     SEND_EMAIL_ERROR(BAD_REQUEST, false, "이메일 인증 코드 전송을 실패하였습니다."),
     COMMENT_NOT_FOUND(NOT_FOUND, false, "댓글을 찾을 수 없습니다"),
     BLOCKUSER_NOT_FOUND(NOT_FOUND, false, "해당 차단 목록을 찾을 수 없습니다."),
-    INVALID_FILE_UPLOAD(BAD_REQUEST, false, "파일 업로드에 실패하였습니다.");
+    INVALID_FILE_UPLOAD(BAD_REQUEST, false, "파일 업로드에 실패하였습니다."),
+    INVALID_FORMAT(BAD_REQUEST, false, "형식에 맞지 않습니다.");
 
     private final int code;
     private final boolean isSuccess;
--- src/main/java/com/modugarden/common/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/modugarden/common/error/exception/GlobalExceptionHandler.java
@@ -5,8 +5,15 @@ import com.modugarden.common.error.exception.custom.LoginCancelException;
 import com.modugarden.common.response.BaseResponseDto;
 import com.modugarden.common.error.enums.ErrorMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.modugarden.common.error.enums.ErrorMessage.*;
 
 @Slf4j
 @RestControllerAdvice
@@ -22,6 +29,19 @@ public class GlobalExceptionHandler {
     public BaseResponseDto<ErrorMessage> loginCancelExceptionHandle(BusinessException e) {
         log.warn("loginCancelException : {}", e);
         return new BaseResponseDto(e.getErrorMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseResponseDto<ErrorMessage> MethodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<String> errorList = bindingResult.getFieldErrors().stream().map(err -> err.getDefaultMessage()).collect(Collectors.toList());
+        System.out.println("errorList = " + errorList);
+        String errorMsg = String.join(" | ", errorList);
+        System.out.println("errorMsg = " + errorMsg);
+
+        log.warn("MethodArgumentNotValidExceptionException : {}", errorMsg);
+
+        return new BaseResponseDto(INVALID_FORMAT);
     }
 
 }
