4c98144621a7faa0e1de0cfeb8525979cad74184
kyeong-hyeok <minkh4227@naver.com>
2023-01-26 21:11:08 +0900

    [ADD] Swagger 설정

--- src/main/java/com/modugarden/ModugardenApplication.java
+++ src/main/java/com/modugarden/ModugardenApplication.java
@@ -2,7 +2,15 @@ package com.modugarden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -13,6 +21,12 @@ public class ModugardenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModugardenApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/swagger"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
 	}
 
 }
