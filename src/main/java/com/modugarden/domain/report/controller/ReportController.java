dd83611c84f31147c5dac1123d512230337a52e2
kyeong-hyeok <minkh4227@naver.com>
2023-01-26 14:17:41 +0900

    [FEAT] 유저 신고 기능 구현

--- src/main/java/com/modugarden/domain/report/controller/ReportController.java
+++ src/main/java/com/modugarden/domain/report/controller/ReportController.java
@@ -1,4 +1,28 @@
 package com.modugarden.domain.report.controller;
 
+import com.modugarden.common.response.BaseResponseDto;
+import com.modugarden.domain.auth.entity.ModugardenUser;
+import com.modugarden.domain.report.dto.response.ReportUserResponseDto;
+import com.modugarden.domain.report.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/reports")
 public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/users/{userId}")
+    public BaseResponseDto<ReportUserResponseDto> reportUser(@PathVariable Long userId,
+                                                             @AuthenticationPrincipal ModugardenUser user) {
+        return new BaseResponseDto<>(reportService.reportUser(user.getUser(), userId));
+    }
 }
--- src/main/java/com/modugarden/domain/report/dto/response/ReportUserResponseDto.java
+++ src/main/java/com/modugarden/domain/report/dto/response/ReportUserResponseDto.java
@@ -1,4 +1,11 @@
 package com.modugarden.domain.report.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ReportUserResponseDto {
+    private Long userId;
+    private Long reportUserId;
 }
