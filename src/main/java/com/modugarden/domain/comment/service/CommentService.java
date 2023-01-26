4ffb9d0237b5e62815959210952194afbdd9070d
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-01-26 14:19:23 +0900

    [ADD/#18] 팔로우관련 경혁오빠꺼 코드 추가

--- src/main/java/com/modugarden/domain/comment/service/CommentService.java
+++ src/main/java/com/modugarden/domain/comment/service/CommentService.java
@@ -13,12 +13,8 @@ import com.modugarden.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
--- src/main/java/com/modugarden/domain/follow/repository/impl/FollowRepositoryImpl.java
+++ src/main/java/com/modugarden/domain/follow/repository/impl/FollowRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.modugarden.domain.follow.repository.impl;
 
 import com.modugarden.domain.follow.repository.FollowRepository;
-import com.querydsl.core.QueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,7 @@ import static com.modugarden.domain.follow.entity.QFollow.follow;
 
 @Repository
 @RequiredArgsConstructor
-public class FollowRepositoryImpl implements FollowRepository {
+public abstract class FollowRepositoryImpl implements FollowRepository {
     private final JPAQueryFactory queryFactory;
     public boolean exists(Long userId, Long followingUserId) {
         Integer fetchOne = queryFactory.selectOne().from(follow).where((follow.user.id.eq(userId)).and(follow.followingUser.id.eq(followingUserId))).fetchFirst();
