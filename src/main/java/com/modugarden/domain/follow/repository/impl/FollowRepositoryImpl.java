5e650b6715707be837b302fa73213fd8b37d18c3
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-29 05:12:12 +0900

    [DEL] 필요없는 repository 클래스 삭제

--- src/main/java/com/modugarden/domain/follow/repository/impl/FollowRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.modugarden.domain.follow.repository.impl;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@RequiredArgsConstructor
-public class FollowRepositoryImpl {  //추상클래스라고 해도 되나?
-}
