f8c5a32e2f4fcc05aa501f29b2d0b8492f8a970c
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-30 00:00:30 +0900

    [ADD/#19] jwtFilter 에러 핸들링 코드 추가

--- src/main/java/com/modugarden/utils/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/modugarden/utils/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.modugarden.utils.jwt;
 
+import com.modugarden.common.error.exception.custom.BusinessException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -8,12 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.modugarden.common.error.enums.ErrorMessage.FORBIDDEN_USER;
+
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         //필요한 권한이 없이 접근하려 할때 403
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
+        throw new BusinessException(FORBIDDEN_USER);
+        //response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
     }
 }
--- src/main/java/com/modugarden/utils/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/modugarden/utils/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.modugarden.utils.jwt;
 
+import com.modugarden.common.error.exception.custom.BusinessException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static com.modugarden.common.error.enums.ErrorMessage.UNAUTHORIZED_USER;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -16,7 +19,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증되지 않은 유저입니다.");
+        throw new BusinessException(UNAUTHORIZED_USER);
+        //response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증되지 않은 유저입니다.");
     }
 
 
