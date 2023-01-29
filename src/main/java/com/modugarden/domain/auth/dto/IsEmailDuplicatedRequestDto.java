0b5a89a0d03abed1cd9d9702b85be767f41cd2c2
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-30 00:01:22 +0900

    [ADD] @Valid 코드 추가

--- src/main/java/com/modugarden/domain/auth/dto/IsEmailDuplicatedRequestDto.java
+++ src/main/java/com/modugarden/domain/auth/dto/IsEmailDuplicatedRequestDto.java
@@ -2,7 +2,12 @@ package com.modugarden.domain.auth.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class IsEmailDuplicatedRequestDto {
+    @Email(message="이메일 형식에 맞지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
 }
