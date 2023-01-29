befc9117c4c89d6396b239b5aa114467ec02bf12
kyeong-hyeok <minkh4227@naver.com>
2023-01-29 22:27:16 +0900

    [ADD] UpdateUserCategoryRequestDto Validation 추가

--- src/main/java/com/modugarden/domain/user/dto/request/UpdateUserCategoryRequestDto.java
+++ src/main/java/com/modugarden/domain/user/dto/request/UpdateUserCategoryRequestDto.java
@@ -2,9 +2,11 @@ package com.modugarden.domain.user.dto.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 public class UpdateUserCategoryRequestDto {
+    @NotNull
     private List<String> categories;
 }
