9e0b872189c8d0677db3addff6bea6b243376217
kyeong-hyeok <minkh4227@naver.com>
2023-02-03 17:00:38 +0900

    [ADD] 테스트 코드 추가

--- src/main/java/com/modugarden/domain/user/controller/UserController.java
+++ src/main/java/com/modugarden/domain/user/controller/UserController.java
@@ -92,4 +92,10 @@ public class UserController {
     public BaseResponseDto<UserNotificationResponseDto> readUserNotification(@AuthenticationPrincipal ModugardenUser user) {
         return new BaseResponseDto<>(userService.readUserNotification(user.getUser()));
     }
+
+    @ApiOperation(value = "배포", notes = "내 프로필 - 알림 설정 - 현재 알림 설정을 조회한다.")
+    @GetMapping("/me/baepo")
+    public BaseResponseDto<UserNotificationResponseDto> readUseraa(@AuthenticationPrincipal ModugardenUser user) {
+        return new BaseResponseDto<>(userService.readUserNotification(user.getUser()));
+    }
 }
