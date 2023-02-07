c45ea0398179d81345b6fcc948be6d1b0d6f2645
kyeong-hyeok <minkh4227@naver.com>
2023-02-07 13:50:42 +0900

    [ADD] 큐레이션 삭제 - 큐레이션 신고 모두 삭제 추가

--- src/main/java/com/modugarden/domain/report/repository/ReportCurationRepository.java
+++ src/main/java/com/modugarden/domain/report/repository/ReportCurationRepository.java
@@ -3,5 +3,9 @@ package com.modugarden.domain.report.repository;
 import com.modugarden.domain.report.entity.CurationReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReportCurationRepository extends JpaRepository<CurationReport, Long> {
+
+    Optional<CurationReport> deleteAllByReportCuration_Id(Long curationId);
 }
