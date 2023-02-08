23d35c68cb40f7fb08f37541b9edad8c63e861d9
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-02-08 13:09:48 +0900

    [ADD] 팔로우추천 response에 팔로우 여부 추가

--- src/main/java/com/modugarden/domain/follow/dto/FollowRecommendResponseDto.java
+++ src/main/java/com/modugarden/domain/follow/dto/FollowRecommendResponseDto.java
@@ -12,4 +12,13 @@ public class FollowRecommendResponseDto {
     private String nickname;
     private String profileImage;
     private List<String> categories;
+    private Boolean isFollow;
+
+    public FollowRecommendResponseDto(Long userId, String nickname, String profileImage, List<String> categories) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.profileImage = profileImage;
+        this.categories = categories;
+        this.isFollow = false; // 쿼리상 내가 팔로잉하고 있는 유저는 추천에서 제외
+    }
 }
