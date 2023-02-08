ec0c9f8a77d1da56eaf0410d3dafcb39c3339d70
kyeong-hyeok <minkh4227@naver.com>
2023-02-08 21:28:17 +0900

    [MOD] findAllByBoard의 인자를 Board로 변경

--- src/main/java/com/modugarden/domain/board/repository/BoardImageRepository.java
+++ src/main/java/com/modugarden/domain/board/repository/BoardImageRepository.java
@@ -1,5 +1,6 @@
 package com.modugarden.domain.board.repository;
 
+import com.modugarden.domain.board.entity.Board;
 import com.modugarden.domain.board.entity.BoardImage;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -11,7 +12,7 @@ import java.util.Optional;
 
 @Repository
 public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
-    List<BoardImage> findAllByBoard_Id(Long board_id);
+    List<BoardImage> findAllByBoard(Board board);
 
     Slice<BoardImage> findAllByUserid(Long user_id, Pageable pageable);
 
