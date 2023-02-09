65a98c033df7ad0cee42d04468316857531bee1d
kyeong-hyeok <minkh4227@naver.com>
2023-02-09 23:37:00 +0900

    [ADD] 회원 차단시 보관된 큐레이션에서 삭제하는 쿼리 추가

--- src/main/java/com/modugarden/domain/storage/entity/repository/CurationStorageRepository.java
+++ src/main/java/com/modugarden/domain/storage/entity/repository/CurationStorageRepository.java
@@ -3,9 +3,13 @@ package com.modugarden.domain.storage.entity.repository;
 import com.modugarden.domain.curation.entity.Curation;
 import com.modugarden.domain.storage.entity.CurationStorage;
 import com.modugarden.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +19,13 @@ public interface CurationStorageRepository extends JpaRepository<CurationStorage
     Optional<CurationStorage> deleteAllByCuration(Curation curation);
 
     Long deleteByUser(User user);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM CurationStorage cs \n" +
+            "WHERE :user_id = cs.user.id and cs IN \n" +
+            "(SELECT cs FROM CurationStorage cs \n" +
+            "LEFT JOIN cs.curation c \n" +
+            "WHERE :block_user_id = c.user.id)")
+    void deleteAllByUser_Id(@Param("user_id") Long user_id, @Param("block_user_id") Long block_user_id);
 }
