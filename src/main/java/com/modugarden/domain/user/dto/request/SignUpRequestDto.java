a9f8134eda313ce1e6370a98ba6400800c7ef78f
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-02-09 20:36:10 +0900

    [MOD] validation 메세지 변경

--- src/main/java/com/modugarden/domain/user/dto/request/SignUpRequestDto.java
+++ src/main/java/com/modugarden/domain/user/dto/request/SignUpRequestDto.java
@@ -18,7 +18,7 @@ public class SignUpRequestDto {
 
 
     @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+            message = "비밀번호는 영문과 숫자가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
