461cca98c0309e0635fe163345e63394d3fecd22
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-02-16 02:20:04 +0900

    [ADD] 로그인시 유저의 권한 반환 추가

--- src/main/java/com/modugarden/domain/auth/dto/response/LoginResponseDto.java
+++ src/main/java/com/modugarden/domain/auth/dto/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.modugarden.domain.auth.dto.response;
 
+import com.modugarden.domain.user.entity.enums.UserAuthority;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,16 +9,17 @@ public class LoginResponseDto {
     private Long userId;
     private String nickname;
     private String profileImage;
-
+    private UserAuthority userAuthority;
     private String accessToken;
     private String refreshToken;
     private String accessToken_expiredDate;
 
     @Builder
-    public LoginResponseDto(Long userId, String nickname, String profileImage, String accessToken, String refreshToken, String accessToken_expiredDate) {
+    public LoginResponseDto(Long userId, String nickname, String profileImage, String accessToken, String refreshToken, String accessToken_expiredDate, UserAuthority userAuthority) {
         this.userId = userId;
         this.nickname = nickname;
         this.profileImage = profileImage;
+        this.userAuthority = userAuthority;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
         this.accessToken_expiredDate = accessToken_expiredDate;
